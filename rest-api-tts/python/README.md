2c613148b409bb1ca2e4fe35684faa0950858701
authorfu <fujiayi2012@gmail.com>
2018-08-29 13:17:06 +0800

    完成python rest api 合成

---
+++ rest-api-tts/python/README.md
@@ -0,0 +1,56 @@
+## 简介
+
+使用python脚本方式测试rest api 识别接口
+
+- 支持python 2.7 及 python 3.7+
+
+
+
+## 测试流程
+
+### 修改tts.py
+
+从网页中申请的应用获取appKey和appSecret
+
+```python
+# 填写网页上申请的appkey 如 API_KEY="g8eBUMSokVB1BHGmgxxxxxx"
+API_KEY = '4E1BG9lTnlSeIf1NQFlrxxxx'
+
+# 填写网页上申请的APP SECRET 如 SECRET_KEY="94dc99566550d87f8fa8ece112xxxxx"
+SECRET_KEY = '544ca4657ba8002e3dea3ac2f5fxxxxx'
+```
+
+
+
+
+
+
+## 运行 tts.py，进行合成
+
+命令为 python tts.py
+
+结果在result.mp3，如果遇见错误，结果在error.txt
+
+其中
+
+- Content-Type: audio/mp3，表示合成成功，可以播放MP3 result.mp3
+- Content-Type: application/json 表示错误   error.txt打开可以看到错误信息的json
+
+### 修改合成参数
+
+```python
+TEXT = "欢迎使用百度语音";
+
+#发音人选择, 0为普通女声，1为普通男生，3为情感合成-度逍遥，4为情感合成-度丫丫，默认为普通女声
+PER = 0;
+#语速，取值0-9，默认为5中语速
+SDP = 5;
+#音调，取值0-9，默认为5中语调
+PIT = 5;
+#音量，取值0-9，默认为5中音量
+VOL = 5;
+
+CUID = "123456PYTHON";
+
+```
+
---
+++ rest-api-tts/python/tts.py
@@ -0,0 +1,119 @@
+# coding=utf-8
+
+import sys
+import json
+
+IS_PY3 = sys.version_info.major == 3
+if IS_PY3:
+    from urllib.request import urlopen
+    from urllib.request import Request
+    from urllib.error import URLError
+    from urllib.parse import urlencode
+    from urllib.parse import quote_plus
+else:
+    import urllib2
+    from urllib import quote_plus
+    from urllib2 import urlopen
+    from urllib2 import Request
+    from urllib2 import URLError
+    from urllib import urlencode
+
+API_KEY = '4E1BG9lTnlSeIf1NQFlrSq6h'
+SECRET_KEY = '544ca4657ba8002e3dea3ac2f5fdd241'
+
+TEXT = "欢迎使用百度语音合成。";
+
+# 发音人选择, 0为普通女声，1为普通男生，3为情感合成-度逍遥，4为情感合成-度丫丫，默认为普通女声
+PER = 4
+# 语速，取值0-15，默认为5中语速
+SPD = 5
+# 音调，取值0-15，默认为5中语调
+PIT = 5
+# 音量，取值0-9，默认为5中音量
+VOL = 5
+# 下载的文件格式, 3：mp3(default) 4： pcm-16k 5： pcm-8k 6. wav
+AUE = 3
+
+FORMATS = {3: "mp3", 4: "pcm", 5: "pcm", 6: "wav"}
+FORMAT = FORMATS[AUE]
+
+CUID = "123456PYTHON"
+
+TTS_URL = 'http://tsn.baidu.com/text2audio'
+
+
+class DemoError(Exception):
+    pass
+
+
+"""  TOKEN start """
+
+TOKEN_URL = 'http://openapi.baidu.com/oauth/2.0/token'
+SCOPE = 'audio_tts_post'  # 有此scope表示有asr能力，没有请在网页里勾选
+
+
+def fetch_token():
+    print("fetch token begin")
+    params = {'grant_type': 'client_credentials',
+              'client_id': API_KEY,
+              'client_secret': SECRET_KEY}
+    post_data = urlencode(params)
+    if (IS_PY3):
+        post_data = post_data.encode('utf-8')
+    req = Request(TOKEN_URL, post_data)
+    try:
+        f = urlopen(req, timeout=5)
+        result_str = f.read()
+    except URLError as err:
+        print('token http response http code : ' + str(err.code))
+        result_str = err.read()
+    if (IS_PY3):
+        result_str = result_str.decode()
+
+    print(result_str)
+    result = json.loads(result_str)
+    print(result)
+    if ('access_token' in result.keys() and 'scope' in result.keys()):
+        if not SCOPE in result['scope'].split(' '):
+            raise DemoError('scope is not correct')
+        print('SUCCESS WITH TOKEN: %s ; EXPIRES IN SECONDS: %s' % (result['access_token'], result['expires_in']))
+        return result['access_token']
+    else:
+        raise DemoError('MAYBE API_KEY or SECRET_KEY not correct: access_token or scope not found in token response')
+
+
+"""  TOKEN end """
+
+if __name__ == '__main__':
+    token = fetch_token()
+    tex = quote_plus(TEXT)  # 此处TEXT需要两次urlencode
+    print(tex)
+    params = {'tok': token, 'tex': tex, 'per': PER, 'spd': SPD, 'pit': PIT, 'vol': VOL, 'aue': AUE, 'cuid': CUID,
+              'lan': 'zh', 'ctp': 1}  # lan ctp 固定参数
+
+    data = urlencode(params)
+    print('test on Web Browser' + TTS_URL + '?' + data)
+
+    req = Request(TTS_URL, data.encode('utf-8'))
+
+    has_error = False
+    try:
+        f = urlopen(req)
+        result_str = f.read()
+
+        has_error = ('Content-Type' not in f.headers.keys() or f.headers['Content-Type'].find('audio/') < 0)
+    except  URLError as err:
+        print('asr http response http code : ' + str(err.code))
+        result_str = err.read()
+        has_error = True
+
+    save_file = "error.txt" if has_error else 'result.' + FORMAT
+    with open(save_file, 'wb') as of:
+        of.write(result_str)
+
+    if has_error:
+        if (IS_PY3):
+            result_str = str(result_str, 'utf-8')
+        print("tts api  error:" + result_str)
+
+    print("result saved as :" + save_file)
