2d577f60095f2c5bf9cf561c4187914a5055aab6
authorfu <fujiayi2012@gmail.com>
2018-08-29 13:12:46 +0800

    完成python rest api

---
+++ rest-api-asr/python/README.md
@@ -0,0 +1,76 @@
+## 简介
+
+使用python 脚本方式测试rest api 识别接口
+
+**支持 python 2.7 及 python 3.7+**
+
+
+
+## 测试流程
+
+选择asr_json.py或者asr_raw.py之一进行测试，本文以asr_json.py为例，以下文档同样适用于asr_raw.py
+
+
+
+### 修改asr_json.py
+
+从网页中申请的应用获取appKey和appSecret
+
+```python
+# 填写网页上申请的appkey 如 API_KEY="g8eBUMSokVB1BHGmgxxxxxx"
+API_KEY = '4E1BG9lTnlSeIf1NQFlrxxxx'
+
+# 填写网页上申请的APP SECRET 如 SECRET_KEY="94dc99566550d87f8fa8ece112xxxxx"
+SECRET_KEY = '544ca4657ba8002e3dea3ac2f5fxxxxx'
+```
+
+
+
+
+## 运行asr_json.py，进行识别
+
+命令为 python asr_json.py
+
+
+
+结果如：
+```json
+{"corpus_no":"6595003755536106531","err_msg":"success.","err_no":0,"result":["北京科技馆，"],"sn":"611278720461535518969"}
+```
+
+windows 下显示可能产生乱码，请打开result.txt
+
+### 测试其它音频文件
+
+
+
+修改以下参数：
+
+```python
+# 需要识别的文件
+AUDIO_FILE = "./16k.pcm";
+# 文件格式
+FORMAT = "pcm"; # 文件后缀 pcm/wav/amr
+# 根据文档填写PID，选择语言及识别模型
+DEV_PID = 1537; #  1537 表示识别普通话，使用输入法模型。1536表示识别普通话，使用搜索模型
+```
+
+
+
+1. 如测试英语 修改为:
+
+```python
+DEV_PID = 1737;
+```
+
+2. 如测试采样率为16k 的amr文件16k-23850.amr，修改为：
+
+```python
+# 需要识别的文件
+AUDIO_FILE = "./16k-23850.amr";
+# 文件格式
+FORMAT = "amr"; // 文件后缀 pcm/wav/amr
+
+```
+
+   
---
+++ rest-api-asr/python/asr_raw.py
@@ -0,0 +1,115 @@
+# coding=utf-8
+
+import sys
+import json
+import base64
+
+IS_PY3 = sys.version_info.major == 3
+
+if IS_PY3:
+    from urllib.request import urlopen
+    from urllib.request import Request
+    from urllib.error import URLError
+    from urllib.parse import urlencode
+else:
+    from urllib2 import urlopen
+    from urllib2 import Request
+    from urllib2 import URLError
+    from urllib import urlencode
+
+API_KEY = '4E1BG9lTnlSeIf1NQFlrSq6h'
+SECRET_KEY = '544ca4657ba8002e3dea3ac2f5fdd241'
+
+# 需要识别的文件
+AUDIO_FILE = './pcm/16k.pcm'
+# 文件格式
+FORMAT = 'wav';  # 文件后缀 pcm/wav/amr
+
+# 根据文档填写PID，选择语言及识别模型
+DEV_PID = 1537;  # 1537 表示识别普通话，使用输入法模型。1536表示识别普通话，使用搜索模型
+
+CUID = '123456PYTHON';
+# 采样率
+RATE = 16000;  # 固定值
+
+ASR_URL = 'http://vop.baidu.com/server_api'
+
+
+class DemoError(Exception):
+    pass
+
+
+"""  TOKEN start """
+
+TOKEN_URL = 'http://openapi.baidu.com/oauth/2.0/token'
+SCOPE = 'audio_voice_assistant_get'  # 有此scope表示有asr能力，没有请在网页里勾选
+
+
+def fetch_token():
+    params = {'grant_type': 'client_credentials',
+              'client_id': API_KEY,
+              'client_secret': SECRET_KEY}
+    post_data = urlencode(params)
+    if (IS_PY3):
+        post_data = post_data.encode( 'utf-8')
+    req = Request(TOKEN_URL, post_data)
+    try:
+        f = urlopen(req)
+        result_str = f.read()
+    except URLError as err:
+        print('token http response http code : ' + str(err.code))
+        result_str = err.read()
+    if (IS_PY3):
+        result_str =  result_str.decode()
+
+    print(result_str)
+    result = json.loads(result_str)
+    print(result)
+    if ('access_token' in result.keys() and 'scope' in result.keys()):
+        if not SCOPE in result['scope'].split(' '):
+            raise DemoError('scope is not correct')
+        print('SUCCESS WITH TOKEN: %s ; EXPIRES IN SECONDS: %s' % (result['access_token'], result['expires_in']))
+        return result['access_token']
+    else:
+        raise DemoError('MAYBE API_KEY or SECRET_KEY not correct: access_token or scope not found in token response')
+
+
+"""  TOKEN end """
+
+if __name__ == '__main__':
+    token = fetch_token()
+
+    speech_data = []
+    with open(AUDIO_FILE, 'rb') as speech_file:
+        speech_data = speech_file.read()
+    length = len(speech_data)
+    if length == 0:
+        raise DemoError('file %s length read 0 bytes' % AUDIO_FILE)
+    speech = base64.b64encode(speech_data)
+    if (IS_PY3):
+        speech = str(speech, 'utf-8')
+    params = {'dev_pid': DEV_PID,
+              'format': FORMAT,
+              'rate': RATE,
+              'token': token,
+              'cuid': CUID,
+              'channel': 1,
+              'speech': speech,
+              'len': length
+              }
+    post_data = json.dumps(params, sort_keys=False)
+    # print post_data
+    req = Request(ASR_URL, post_data.encode('utf-8'))
+    req.add_header('Content-Type', 'application/json')
+    try:
+        f = urlopen(req)
+        result_str = f.read()
+    except  URLError as err:
+        print('asr http response http code : ' + str(err.code))
+        result_str = err.read()
+
+    if (IS_PY3):
+        result_str = str(result_str, 'utf-8')
+    print(result_str)
+    with open("result.txt","w") as of:
+        of.write(result_str)
