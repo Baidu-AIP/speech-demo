eab1409bd76ea48c325aacab0b10ba8525bf6e32
franklyn867 <franklyn867@gmail.com>
2019-07-08 14:58:57 +0800

    Update asr_json.php

--- rest-api-asr/php/asr_json.php
+++ rest-api-asr/php/asr_json.php
@@ -21,16 +21,16 @@ $CUID = "123456PHP";
 # 采样率
 $RATE = 16000;  // 固定值
 
-// 普通版
+# 普通版
 $ASR_URL = "http://vop.baidu.com/server_api";
 # 根据文档填写PID，选择语言及识别模型
 $DEV_PID = 1537; //  1537 表示识别普通话，使用输入法模型。1536表示识别普通话，使用搜索模型
 $SCOPE = 'audio_voice_assistant_get'; // 有此scope表示有语音识别普通版能力，没有请在网页里开通语音识别能力
 
-// 极速版打开以下信息 打开注释的话请填写自己申请的appkey appSecret ，并在网页中开通极速版
-$ASR_URL = "https://vop.baidu.com/pro_api";
-$DEV_PID = 80001; 
-$SCOPE = 'brain_enhanced_asr';  // 有此scope表示有极速版能力，没有请在网页里开通极速版
+# 极速版打开以下信息 打开注释的话请填写自己申请的appkey appSecret ，并在网页中开通极速版（开通后可能会收费）
+//$ASR_URL = "https://vop.baidu.com/pro_api";
+//$DEV_PID = 80001; 
+//$SCOPE = 'brain_enhanced_asr';  // 有此scope表示有极速版能力，没有请在网页里开通极速版
 
 $SCOPE = false; // 部分历史应用没有加入scope，设为false忽略检查
 
