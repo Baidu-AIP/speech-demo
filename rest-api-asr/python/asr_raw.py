669c80b0d59a390b5dd08c821923b043dee2a4b5
franklyn867 <franklyn867@gmail.com>
2019-07-08 17:01:51 +0800

    Update asr_raw.py

--- rest-api-asr/python/asr_raw.py
+++ rest-api-asr/python/asr_raw.py
@@ -30,21 +30,21 @@ API_KEY = 'kVcnfD9iW2XVZSMaLMrtLYIz'
 SECRET_KEY = 'O9o1O213UgG5LFn0bDGNtoRN3VWl2du6'
 
 # 需要识别的文件
-AUDIO_FILE = './pcm/16k.pcm'  # 只支持 pcm/wav/amr
+AUDIO_FILE = './pcm/16k.pcm'  # 只支持 pcm/wav/amr 格式，极速版额外支持m4a 格式
 # 文件格式
-FORMAT = AUDIO_FILE[-3:];  # 文件后缀只支持 pcm/wav/amr
+FORMAT = AUDIO_FILE[-3:];  # 文件后缀只支持 pcm/wav/amr 格式，极速版额外支持m4a 格式
 
 CUID = '123456PYTHON';
 # 采样率
 RATE = 16000;  # 固定值
 
-# 免费版
+# 普通版
 
 DEV_PID = 1536;  # 1537 表示识别普通话，使用输入法模型。1536表示识别普通话，使用搜索模型。根据文档填写PID，选择语言及识别模型
 ASR_URL = 'http://vop.baidu.com/server_api'
 SCOPE = 'audio_voice_assistant_get'  # 有此scope表示有asr能力，没有请在网页里勾选，非常旧的应用可能没有
 
-# 收费极速版 打开注释的话请填写自己申请的appkey appSecret ，并在网页中开通极速版
+# 极速版 打开注释的话请填写自己申请的appkey appSecret ，并在网页中开通极速版（开通后可能会收费）
 
 #DEV_PID = 80001
 #ASR_URL = 'https://vop.baidu.com/pro_api'
@@ -54,7 +54,7 @@ SCOPE = 'audio_voice_assistant_get'  # 有此scope表示有asr能力，没有请
 # SCOPE = False
 
 
-# 收费极速版
+# 极速版
 
 class DemoError(Exception):
     pass
