e92425290ce139db09e05f0eab3e98bfd7d63321
franklyn867 <franklyn867@gmail.com>
2020-02-29 19:15:26 +0800

    Update asr_raw.py
    remove 1536

--- rest-api-asr/python/asr_raw.py
+++ rest-api-asr/python/asr_raw.py
@@ -40,7 +40,7 @@ RATE = 16000;  # 固定值
 
 # 普通版
 
-DEV_PID = 1537;  # 1537 表示识别普通话，使用输入法模型。1536表示识别普通话，使用搜索模型。根据文档填写PID，选择语言及识别模型
+DEV_PID = 1537;  # 1537 表示识别普通话，使用输入法模型。根据文档填写PID，选择语言及识别模型
 ASR_URL = 'http://vop.baidu.com/server_api'
 SCOPE = 'audio_voice_assistant_get'  # 有此scope表示有asr能力，没有请在网页里勾选，非常旧的应用可能没有
 
