5a93ffade9e571a6629ddb7d303ec06cabacfa42
franklyn867 <franklyn867@gmail.com>
2023-06-26 16:23:52 +0800

    Update asr_json.php
    Delete authentication information

--- rest-api-asr/php/asr_json.php
+++ rest-api-asr/php/asr_json.php
@@ -6,10 +6,10 @@ define('DEMO_CURL_VERBOSE', false); // 打印curl debug信息
 */
 
 # 填写网页上申请的appkey 如 $apiKey="g8eBUMSokVB1BHGmgxxxxxx"
-$API_KEY =  "kVcnfD9iW2XVZSMaLMrtLYIz";
+$API_KEY =  "kVcnfD9iW2XVZSMaLxxxxx";
 
 # 填写网页上申请的APP SECRET 如 $secretKey="94dc99566550d87f8fa8ece112xxxxx"
-$SECRET_KEY = "O9o1O213UgG5LFn0bDGNtoRN3VWl2du6";
+$SECRET_KEY = "O9o1O213UgG5LFn0bDGNtoRNxxxxx";
 
 # 需要识别的文件
 $AUDIO_FILE = "./16k.pcm";
