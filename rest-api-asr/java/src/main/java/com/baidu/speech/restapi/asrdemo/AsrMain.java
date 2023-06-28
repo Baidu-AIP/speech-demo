3f45e1718c770faa9d1fb1575f2fe68450ae9782
franklyn867 <franklyn867@gmail.com>
2023-06-28 17:23:02 +0800

    Update AsrMain.java
    Delete authentication information

--- rest-api-asr/java/src/main/java/com/baidu/speech/restapi/asrdemo/AsrMain.java
+++ rest-api-asr/java/src/main/java/com/baidu/speech/restapi/asrdemo/AsrMain.java
@@ -18,10 +18,10 @@ public class AsrMain {
     private final boolean METHOD_RAW = false; // 默认以json方式上传音频文件
 
     //  填写网页上申请的appkey 如 $apiKey="g8eBUMSokVB1BHGmgxxxxxx"
-    private final String APP_KEY = "kVcnfD9iW2XVZSMaLMrtLYIz";
+    private final String APP_KEY = "kVcnfD9iW2XVZSMaLXXXXXXX";
 
     // 填写网页上申请的APP SECRET 如 $SECRET_KEY="94dc99566550d87f8fa8ece112xxxxx"
-    private final String SECRET_KEY = "O9o1O213UgG5LFn0bDGNtoRN3VWl2du6";
+    private final String SECRET_KEY = "O9o1O213UgG5LFn0bDGNtoRNXXXXXX";
 
     // 需要识别的文件
     private final String FILENAME = "16k.pcm";
